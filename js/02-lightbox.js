ff7ba86fe089113f4b77636337fbefeb4539b950
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-10-20 21:27:11 +0300

    ex2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML("afterbegin", creatMarkup(galleryItems));
+
+//creation HTML
+function creatMarkup(arr) {
+    return arr.map(({ preview, original, description}) =>
+   `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('')
+  };
+
+
+  const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: "250ms",
+    overlayOpacity: "0.5",
+    
+  });
+
 console.log(galleryItems);
+
